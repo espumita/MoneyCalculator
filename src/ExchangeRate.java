467822a8bb9d656fe35a87db5a6b94d8cba59a2c
David Falcón Naranjo <david_el_125@hotmail.com>
2015-11-10 22:32:12 +0000

    Project first commit

---
+++ src/ExchangeRate.java
@@ -0,0 +1,23 @@
+public class ExchangeRate {
+    private final Currency from;
+    private final Currency to;
+    private final double value;
+
+    public ExchangeRate(Currency from, Currency to, double value) {
+        this.from = from;
+        this.to = to;
+        this.value = value;
+    }
+
+    public Currency getFrom() {
+        return from;
+    }
+
+    public Currency getTo() {
+        return to;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
